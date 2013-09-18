50b76fc9d8e67b82f7ab037624805434c83a125c
mariokeller <mariokeller@Marios-iMac.local>
2013-09-18 15:49:27 +0200

    edit

--- start.php
+++ start.php
@@ -26,23 +26,7 @@ include "php/functions.php";
         ?>
         <link rel="stylesheet" href="css/style.css" media="screen,projection">
         <script src="js/libs/modernizr-2.0.6.min.js"></script>
-        <script>
-            (function(i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function() {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                        m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-40911491-1', 'paperly.de');
-            ga('send', 'pageview');
-
-        </script>
+     
         <!-- template functions -->
         <script src="js/libs/template/templateClass.js"></script>
         <?php
